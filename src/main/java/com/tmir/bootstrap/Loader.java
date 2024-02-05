64a70df31011f331cc591db3b436656364e40159
TimurMironov <mironov.timur.99@mail.ru>
2024-02-06 00:21:33 +0300

    Незначительные исправления

--- src/main/java/com/tmir/bootstrap/Loader.java
+++ src/main/java/com/tmir/bootstrap/Loader.java
@@ -3,6 +3,7 @@ package com.tmir.bootstrap;
 import com.tmir.entity.Role;
 import com.tmir.entity.User;
 import com.tmir.service.UserService;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -20,7 +21,7 @@ public class Loader implements ApplicationListener<ContextRefreshedEvent> {
 
     // При запуске приложения создается пользователь super с паролем super
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+    public void onApplicationEvent(@NotNull ContextRefreshedEvent event) {
         User user = new User();
         user.setUsername("super");
         user.setPassword("super");
